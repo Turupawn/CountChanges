2f5d89ec3dfb6896ba924b521deab22415310265
Turupawn <ahmed.hn.43@gmail.com>
2017-03-08 17:35:03 -0600

    count changes

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "Test.h"
+#include <iostream>
+using namespace std;
+
+int countSolutions(vector<int> sorted_coins, int amount)
+{
+    return -1;
+}
+
+int main ()
+{
+    test();
+    return 0;
+}
